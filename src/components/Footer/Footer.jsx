18646c4bd5667b537db12c8f19d082394a1394f5
labCap <banlazthosow@gmail.com>
2023-01-22 20:51:16 +0200

    created component Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.scss";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <footer>
+        <div className="container">
+          <div className="inner">
+            <div className="powered">
+              Powered by
+              <a href="https://github.com/labCap" target="_blank">
+                labCap
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </footer>
+  );
+};
